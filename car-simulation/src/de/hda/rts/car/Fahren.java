6e1409ee6f00de91c7bacdd238290b52c83fef0a
sascha.arthur@gmail.com <sascha.arthur@gmail.com>
2013-05-13 17:51:44 +0200

    a

--- car-simulation/src/de/hda/rts/car/Fahren.java
+++ car-simulation/src/de/hda/rts/car/Fahren.java
@@ -1,19 +1,32 @@
 package de.hda.rts.car;
 
+import static de.hda.rts.car.Task.car;
+import static de.hda.rts.car.Task.street;
+
 public class Fahren extends Task
 {
 	public Fahren(long t, int V, int dx,int dy)
 	{
 		super(t);
-		Task.car.V = V;
-		Task.car.Dx = dx;
-		Task.car.Dy = dy;
+		car.V = V;
+		car.Dx = dx;
+		car.Dy = dy;
 	}
 
 	@Override
 	protected void execute()
 	{
-		double realV = (this.period * Task.car.V) / 1000.0;
+		double length = Math.sqrt(car.Dx*car.Dx+ car.Dy*car.Dy);		
+		double realV = (this.period * length) / 1000.0;
+		
+		car.x=+ car.Dx*realV;
+		car.y=+ car.Dy*realV;
+				
+		double entf = street.distance(car.x, car.y);
+		if(entf < 0)
+		{
+			
+		}
 	}
 
 }
